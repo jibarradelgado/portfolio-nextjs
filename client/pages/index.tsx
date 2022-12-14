d72853fcd392d0cd83bb139bfc2be3b39fd88d26
Jorge Ibarra <hello@jorgeibarra.com.mx>
2022-12-14 11:19:12 -0600

    getting assets using fetch method

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,33 @@
+import React, { useEffect } from 'react'
 import Layout from '@components/Layout/Layout'
 import Menu from '@components/Menu/Menu'
 import { Total } from '@components/Total/total'
 import { Asset } from '@components/Asset/Asset'
-import React from 'react'
+
+const query = `
+query {
+	assets {
+		id
+		name
+		value
+		userId
+		assetTypeId
+	}
+}
+`
 
 const HomePage = () => {
-
-  
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_SERVICE_URL}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query
+      })
+    })
+  }, [])
 
   return (
     <Layout title='Home'>
