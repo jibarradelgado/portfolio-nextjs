db3688dbe9852c88f2d37977b9f048d06dc1ccca
Jorge Ibarra <hello@jorgeibarra.com.mx>
2022-12-16 15:20:49 -0600

    Querying using autogenerated code and types

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,42 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useQuery, gql } from '@apollo/client'
 import Layout from '@components/Layout/Layout'
 import Menu from '@components/Menu/Menu'
 import { Total } from '@components/Total/total'
 import { Asset } from '@components/Asset/Asset'
 import { useGetAllAssetsQuery } from 'service/graphql'
-
-const assetFragment = `
-		id
-		name
-		value
-		userId
-		assetTypeId
-`
-
-const useAssets = () => {
-  const query = gql`
-    query getAllAssets {
-      assets {
-        ${assetFragment}
-      }
-    }
-  `
-  return useQuery(query)
-}
-
-const useAsset = (userId: number | string) => {
-  const query = gql`
-    query getAsset($where: AssetWhereInput) {
-      assets (where: $where) {
-        ${assetFragment}
-      }
-    }
-  `
-
-  return useQuery(query, {variables: {where: {userId: userId}}})
-}
-
 
 const HomePage = () => {
   const {data, loading} = useGetAllAssetsQuery()
